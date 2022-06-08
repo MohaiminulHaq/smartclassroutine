de627715fb59b6cfff2e8e6779f9b07522fab501
MohaiminulHaq <mohaiminul35-117@diu.edu.bd>
2022-06-08 21:42:53 +0600

    ok

--- src/main/java/com/my/class_routine/model/StudentDetails.java
+++ src/main/java/com/my/class_routine/model/StudentDetails.java
@@ -1,13 +1,13 @@
 package com.my.class_routine.model;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import javax.persistence.*;
-import java.util.Date;
 
 @Data
-//@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "STUDENT_DETAILS")
-public class StudentDetails {
+public class StudentDetails extends BaseEntity {
 
 
     @Column(name = "Name", unique = true)
@@ -19,32 +19,5 @@ public class StudentDetails {
     @Column(name = "NO_CLASS")
     private Integer noClass;
 
-    private static final long serialVersionUID = 9132875688068247271L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="ID")
-    private Integer id;
-
-    @Column(name = "ACTIVE", columnDefinition = "boolean default true")
-    private Boolean active = true;
-
-    @Column(name = "ENTRY_USER", updatable = false, nullable = false)
-    private Integer entryUser;
-
-    @Column(name = "ENTRY_DATE", updatable = false, nullable = false)
-    private Date entryDate;
-
-    @Column(name = "UPDATE_USER", insertable = false)
-    private Integer updateUser;
-
-    @Column(name = "UPDATE_DATE", insertable = false)
-    private Date updateDate;
-
-    @Column(name = "ENTRY_APP_USER_CODE", length=50)
-    private String entryAppUserCode;
-
-    @Column(name = "UPDATE_APP_USER_CODE", length=50)
-    private String updateAppUserCode;
 
 }
